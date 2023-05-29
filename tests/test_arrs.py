fbe8b08bdd37d6ed7cf550196e3f19d30a333c26
korchaginks <kkonstantinserg@gmail.com>
2023-05-29 23:31:09 +0300

    Дописаны функции test_get, test_my_slice. Покрытие тестами 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -5,6 +5,7 @@ from utils import arrs
 
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
+    assert arrs.get([1, 2, 3], -1, "test") == "test"
 
 
 def test_get__index_error():
@@ -12,6 +13,10 @@ def test_get__index_error():
         arrs.get([], 0, "test")
 
 
-def test_slice():
+def test_my_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], -2) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], -3, -2) == [1]
+    assert arrs.my_slice([]) == []
+    assert arrs.my_slice([1], -3, 0) == []
