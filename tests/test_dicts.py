a55ef7a7cbea99dfeeda243a0173f1724793c015
korchaginks <kkonstantinserg@gmail.com>
2023-05-30 22:57:46 +0300

    Реализовано использование декоратора pytest'a, если необходимо было использовать декоратор для создания списка и вызова его в тестах, прошу написать. Также добавлено небольшое исправление test_dicts.py

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -3,11 +3,11 @@ import pytest
 from utils.dicts import get_val
 
 
-@pytest.mark.parametrize('data, key, default', [
-    ({"word1": 6, 2: "word2"}, 2, "word2"),
-    ({"word1": 6, 2: "word2"}, 0, "empty"),
-    ({}, 2, "None"),
-    ({"key1": "value1", "key2": "value2"}, "key2", "value2")
+@pytest.mark.parametrize('data, key, default, expected', [
+    ({"word1": 6, 2: "word2"}, 2, "None", "word2"),
+    ({"word1": 6, 2: "word2"}, 0, "empty", "empty"),
+    ({}, 2, "None", "None"),
+    ({"key1": "value1", "key2": "value2"}, "key2", "None", "value2")
 ])
-def test_get_val(data: dict, key, default):
-    assert get_val(data, key, default)
+def test_get_val(data: dict, key, default, expected):
+    assert get_val(data, key, default) == expected
